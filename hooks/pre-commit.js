06ffa904a5b78b57e0937690c0642755a060e0bf
Thibault H <Aelto@users.noreply.github.com>
2018-09-15 17:34:19 +0200

    Add quick git hook tasks (#1)
    * add git hooks with husky.js
    * add the pre-commit hook task

---
+++ hooks/pre-commit.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const path = require('path')
+
+const src_directory = path.join(__dirname, '../src')
+
+function cancel_commit(message = '') {
+  console.error(message)
+  process.exit(1)
+}
+
+function recursive_lookup(dir, action = () => {}) {
+  const children = fs.readdirSync(dir)
+
+  for (const child of children) {
+    const child_path = path.join(dir, child)
+
+    if (fs.statSync(child_path).isDirectory()) {
+      recursive_lookup(child_path, action)
+    }
+
+    else {
+      action(child_path)
+    }
+  }
+}
+
+function scan_file(file_path) {
+  const file_content = fs.readFileSync(file_path, 'utf8')
+    .toLowerCase()
+
+  const forbidden_words = [
+    'todo'
+  ]
+
+  const found_words = forbidden_words
+    .filter(word => file_content.includes(word))
+
+  if (found_words.length) {
+    return {
+      file_path,
+      forbidden_words: found_words
+    }
+  }
+
+  return null
+}
+
+function main() {
+  let files_with_error = []
+
+  recursive_lookup(src_directory, (file_path) => {
+    const result = scan_file(file_path)
+
+    if (result !== null) {
+      files_with_error.push(result)
+    }
+  })
+
+  if (files_with_error.length) {
+    const cancel_message = files_with_error
+      .map(error => `forbidden words [${error.forbidden_words.map(word => `"${word}"`).join(', ')}] found in file "${error.file_path}"`)
+
+    cancel_commit(`${cancel_message.join('\n')}\ncommit cancelled.`)
+  }
+}
+
+main()
